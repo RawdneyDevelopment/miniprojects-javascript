9ac6b8063afe3a0e327ded5a95980ec6cd61f527
Rawdney Mendes <mendesrawdney@gmail.com>
2022-10-25 03:15:21 -0300

    tipos de dados primitivos.

--- Tipos_de_Dados/index.js
+++ Tipos_de_Dados/index.js
@@ -13,5 +13,11 @@ var idade = 20 /* Number*/
 var possuiFaculdade = true /* Boolean*/
 var time; /* Undefined*/
 var pesca = null /* Null*/
-var simbolo = Symbol /* Symbol*/
+var simbolo = Symbol() /* Symbol*/
 var objetoNew = {} /* Objeto */
+console.log(typeof nome, idade, possuiFaculdade, time, pesca, simbolo, objetoNew)
+
+
+
+/* Todos eles são primitivos exeto os objetos.
+qual a diferença? os primitivos são imutáveis, não tem como você mudar.*/
