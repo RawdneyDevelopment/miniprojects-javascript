7409c6a3a14ed393c21e7cf70a0863e2c4102b78
Rawdney Mendes <mendesrawdney@gmail.com>
2022-10-25 02:26:28 -0300

    first commit, inicialization javascript

---
+++ Variaveis/index.js
@@ -0,0 +1,10 @@
+/* Variáveis são responsáveis por guardar dados na memória
+inicia com a palavra var, let ou const
+*/
+
+var nome = 'Rawdney'
+console.log(nome);
+let idade = 28
+console.log(idade);
+const possuiFaculdade = true;
+console.log(possuiFaculdade);
