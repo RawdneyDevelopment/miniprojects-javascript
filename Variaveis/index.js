b6cc02f255834b3915a2057385a5d6920336d93e
Rawdney Mendes <mendesrawdney@gmail.com>
2022-10-25 02:46:25 -0300

    organization code to studing

--- Variaveis/index.js
+++ Variaveis/index.js
@@ -6,8 +6,12 @@ var nome = 'Rawdney'
 let idade = 28
 const possuiFaculdade = true;
 console.log(`Meu nome é ${nome}, Minha idade: ${idade}, possuo faculdade? ${possuiFaculdade}`);
-
+/* Você pode fazer operações básicas com variáveis*/
 var preco = 25
 var totalComprado = 5
 var totalPreco = totalComprado * preco;
 console.log(totalPreco);
+/* Você simplesmente pode declarar variáveis assim:*/
+var sobrenome = 'Mendes', 
+cidade = 'Pernambuco';
+console.log(sobrenome, cidade);
