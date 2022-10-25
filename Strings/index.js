3270833ad2ca4c0e3af27deadcbef756483989b2
Rawdney Mendes <mendesrawdney@gmail.com>
2022-10-25 06:49:23 -0300

    added

---
+++ Strings/index.js
@@ -0,0 +1,18 @@
+/* String, você pode concatenar ela*/
+
+var nome = 'Rawdney'
+var sobrenome = 'Mendes'
+var nomeCompleto = nome +  ' ' + sobrenome
+console.log(nomeCompleto);
+
+/* Também pode somar string com números, nesse caso o número vai se transformar em uma string*/
+
+var gols = 100
+var frase = 'Romário fez ' + gols + ' gols'
+console.log(typeof frase)
+
+
+/* ele irá concatenar ambos, então sempre no final irá se transformar em uma string */
+var ano = '2018'
+var mes = 08 
+console.log(ano + mes)
