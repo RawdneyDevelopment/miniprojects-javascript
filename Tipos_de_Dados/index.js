af8b4f06e6ab0698b7bebcf3a1f4701a7e610db6
Rawdney Mendes <mendesrawdney@gmail.com>
2022-10-25 03:10:36 -0300

    tips dados

---
+++ Tipos_de_Dados/index.js
@@ -0,0 +1,17 @@
+/* são 7 diferentes tipos de dados 
+String,
+Number,
+Boolean,
+Undefined,
+Null,
+Symbol,
+Object
+*/
+
+var nome = 'Rawdney' /* String*/
+var idade = 20 /* Number*/
+var possuiFaculdade = true /* Boolean*/
+var time; /* Undefined*/
+var pesca = null /* Null*/
+var simbolo = Symbol /* Symbol*/
+var objetoNew = {} /* Objeto */
