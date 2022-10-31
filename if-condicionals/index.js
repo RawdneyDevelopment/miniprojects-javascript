ddbfe169743224a0afee51743e98b1ab407ac6d2
Rawdney Mendes <mendesrawdney@gmail.com>
2022-10-31 07:42:37 -0300

    if condicionals

--- if-condicionals/index.js
+++ if-condicionals/index.js
@@ -22,3 +22,15 @@ else {
     console.log('Não possui nada');
 }
 /* Sempre em condicionais ele verifica se é boolean, então sempre é uma verificação entre True e false*/
+
+
+/* Existem valores que retornem true e outros que retornaram false quando verificados em uma expressão booleana.*/
+
+/* Falsy */
+
+if(false)
+if(0) // ou -0
+if(NaN)
+if(null)
+if(undefined)
+if('')
