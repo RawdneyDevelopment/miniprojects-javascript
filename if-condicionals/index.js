524f4b5fd567988aa355d5b5b7fa2fd38d16a551
Rawdney Mendes <mendesrawdney@gmail.com>
2022-10-31 10:07:49 -0300

    added: if condicionals

--- if-condicionals/index.js
+++ if-condicionals/index.js
@@ -37,3 +37,31 @@ if('') /* ou "" ou `` */
 
 
 /* todo resto é truthy */
+
+if(true)
+if(1)
+if(' ')
+if('Rawndey')
+if(-5)
+if({})
+
+/* operador lógico de negação 
+o operador !, nega uma operação booleana. ou seja, !true é igual a false.
+*/
+
+if(!true) //false
+if(!1) // false
+if(!'') //true
+if(!undefined) //true
+if(!!' ') //true
+if(!!'') //false
+
+/* 
+Operadores de comparação, vão sempre retornar um valor booleano
+*/
+
+10 > 5 // true
+5 > 10 //false
+20 < 10 //false
+10 <= 10 //true
+10 >= 11 //false
