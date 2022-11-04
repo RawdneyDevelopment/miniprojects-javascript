9997eef04a247e100a94543cc24503d90f17c6b4
Rawdney Mendes <mendesrawdney@gmail.com>
2022-11-04 17:02:12 -0300

    added: examples functions

---
+++ funcoes/index.js
@@ -0,0 +1,27 @@
+/* 
+Funções 
+Bloco de código que pode ser executado e reutilizado.
+Valores podem ser passados por uma função e a mesma retorna outro valor.
+*/
+
+function areaQuadrado(){
+ console.log('Função ativa')
+}
+
+areaQuadrado()
+
+/* Chamada de função declaration */
+
+
+/* se passarmos parametros de uma função, podemos invocar também, exemplo: */
+function areaQuadrado2(lado){
+    return lado
+}
+console.log(areaQuadrado2(2)); // irá retornar 2
+
+
+/* podemos fazer multiplicações, divisões entre outros.*/
+function areaQuadradoMultiplicar(lado){
+    return lado * lado
+}
+console.log(areaQuadradoMultiplicar(4))
