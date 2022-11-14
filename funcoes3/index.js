d436ff04d1568a60da8cddee758fbfcc397df64a
Rawdney Mendes <mendesrawdney@gmail.com>
2022-11-14 00:17:39 -0300

    new functions

---
+++ funcoes3/index.js
@@ -0,0 +1,14 @@
+/* Crie uma função para verificar se o valor é Truthy*/
+
+function isTruthy(dado){
+  return !!dado;
+}
+
+/*Crie uma funçao matemática que retorne o perímetro de um quadrado*/
+function perimetroQuadrado(lado){
+  return lado + lado + lado + lado //ou lado * 4;
+};
+function nomeCompleto(nome, sobrenome){
+  return nome +  ' ' +sobrenome
+}
+
